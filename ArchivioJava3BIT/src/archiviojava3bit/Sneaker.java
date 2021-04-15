eaba8f98792eb29464fc81eb13a051b88a44c0d6
Marco <Marco@192.168.1.13>
2021-04-15 17:12:13 +0200

    Modificato classe sneaker

---
+++ ArchivioJava3BIT/src/archiviojava3bit/Sneaker.java
@@ -0,0 +1,24 @@
+package archiviojava3bit;
+
+/**
+ * Classe utilizzata per l'oggetto da contenere nella collezione.
+ * Contiene le varibili di istanza, il metodo costruttore, i setter/getter
+ * e eventuali altri metodi
+ * @author INSERISCI_NOME
+ */
+public class Sneaker {
+    private String codice; 
+    private float taglia;
+    private String marca;
+    private String modello;
+    private String colore;
+    private float prezzo;
+    private int quantità;
+
+public Sneaker(String codice, float taglia, String marca, String modello, String colore, float prezzo, int quantità){
+
+}
+    
+    
+    
+}
